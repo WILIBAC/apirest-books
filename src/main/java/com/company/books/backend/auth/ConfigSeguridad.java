5c15b17dc00313f68a370d33728c00c0fa3464ea
Wilson Bautista Cuevas <wbautista@aportesenlinea.com>
2022-04-04 11:24:29 -0500

    solución error anotación enable swagger

--- src/main/java/com/company/books/backend/auth/ConfigSeguridad.java
+++ src/main/java/com/company/books/backend/auth/ConfigSeguridad.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 //import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -50,14 +51,13 @@ public class ConfigSeguridad extends WebSecurityConfigurerAdapter{
 	}
 
 
-	/*
-	 * @Override public void configure(WebSecurity web) throws Exception {
-	 * 
-	 * web.ignoring().antMatchers("/v2/api-docs", "/configuration/**",
-	 * "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/api-docs/**");
-	 * 
-	 * }
-	 */
+	@Override public void configure(WebSecurity web) throws Exception {
+	  
+	   web.ignoring().antMatchers("/v2/api-docs", "/configuration/**",
+	   "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/api-docs/**");
+	  
+	}
+	 
 	
 	
 
